48bf7b6a1aeffbb0cf19899faab3099311bac9c2
Marat Zimnurov <zimtir@mail.ru>
2020-07-18 17:28:51 +0300

    Release with improvements (#6)
    * add docke
    * add eslint
    * add prettier
    * add typescript
    * add scss
    * add ts example
    * update a documentation

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,31 @@
-<script>
-  import Spinner from 'svelte-spinner'
-</script>
-
 <style>
-
+  h1 {
+    color: brown;
+  }
 </style>
 
-<Spinner size="50" speed="750" color="#A82124" thickness="2" gap="40" />
+<script lang="typescript">
+  import { onMount } from 'svelte'
+
+  import Spinner from 'svelte-spinner'
+
+  import './App.scss'
+
+  let header: string
+
+  const setHeader = (input: string) => {
+    header = input
+  }
+
+  console.log(Spinner)
+
+  onMount(() => setTimeout(() => setHeader('Hello world'), 2000))
+</script>
+
+<div class="container">
+  {#if header && header.length > 0}
+    <h1 class="container-header">{header}</h1>
+  {:else}
+    <Spinner size="50" speed="750" color="#A82124" thickness="2" gap="40" />
+  {/if}
+</div>
